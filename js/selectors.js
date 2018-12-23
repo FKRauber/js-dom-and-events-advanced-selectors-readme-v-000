d5228c8d152d1c3900ec6c15630bee50ea22bd34
Fabienne Rauber <fabienne.rauber@gmail.com>
2018-12-23 19:48:34 +0000

    Automatically backed up by Learn

--- js/selectors.js
+++ js/selectors.js
@@ -1,6 +1,11 @@
 // declare your functions here...
 $(document).ready(function(){
-  $('img');
-  $('.pics');
-  $('#baby-ninja');
+  $('img');                                   // ELEMENT SELECTOR
+  $('.pics');                                 // CLASS SELECTOR
+  $('#baby-ninja');                           // ID SELECTOR
+  $('ul li');                                 // DESCENDANT SELECTOR
+  $('parent-tag child-tag:first-child');      // FIRST CHILD SELECTOR SYNTAX
+  $('div img:first-child');                   // FIRST CHILD SELECTOR
+  $("img[alt='the beatles making faces']");   // ALT TAG SELECTOR
+  $('div:last');                              // LAST SELECTOR
 });
