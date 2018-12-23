974a1499c21b5a5b1b729b2836b5a12febadcf86
Fabienne Rauber <fabienne.rauber@gmail.com>
2018-12-23 19:50:25 +0000

    Automatically backed up by Learn

--- js/selectors.js
+++ js/selectors.js
@@ -1,11 +1,14 @@
 // declare your functions here...
-$(document).ready(function(){
-  // $('img');                                   // ELEMENT SELECTOR
-  // $('.pics');                                 // CLASS SELECTOR
-  // $('#baby-ninja');                           // ID SELECTOR
-  // $('ul li');                                 // DESCENDANT SELECTOR
-  // $('parent-tag child-tag:first-child');      // FIRST CHILD SELECTOR SYNTAX
-  // $('div img:first-child');                   // FIRST CHILD SELECTOR
-  // $("img[alt='the beatles making faces']");   // ALT TAG SELECTOR
-  // $('div:last');                              // LAST SELECTOR
-});
+
+// $('img');                                   // ELEMENT SELECTOR
+// $('.pics');                                 // CLASS SELECTOR
+// $('#baby-ninja');                           // ID SELECTOR
+// $('ul li');                                 // DESCENDANT SELECTOR
+// $('parent-tag child-tag:first-child');      // FIRST CHILD SELECTOR SYNTAX
+// $('div img:first-child');                   // FIRST CHILD SELECTOR
+// $("img[alt='the beatles making faces']");   // ALT TAG SELECTOR
+// $('div:last');                              // LAST SELECTOR
+
+function paragraphSelector() {
+  $('p');
+}
